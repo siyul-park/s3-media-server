4ed8522d32449727c56173b098aea3a806d9bafd
siyual-park <siyual.bak@gmail.com>
2020-12-03 09:13:48 +0900

    add depenabot

--- packages/s3-media/src/s3/s3-repository-factory.provider.ts
+++ packages/s3-media/src/s3/s3-repository-factory.provider.ts
@@ -3,9 +3,7 @@ import { interfaces } from "cheeket";
 import Token from "./token";
 import S3RepositoryFactory from "./s3-repository-factory";
 
-function s3RepositoryFactoryProvider(): interfaces.Provider<
-  S3RepositoryFactory
-> {
+function s3RepositoryFactoryProvider(): interfaces.Provider<S3RepositoryFactory> {
   return async (context) => {
     return new S3RepositoryFactory(await context.resolve(Token.S3));
   };
