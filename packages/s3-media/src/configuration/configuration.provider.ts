e379b013dc6caa987267bd2348551051dc3668a3
siyual-park <siyual.bak@gmail.com>
2020-12-03 12:37:51 +0900

    add more env

--- packages/s3-media/src/configuration/configuration.provider.ts
+++ packages/s3-media/src/configuration/configuration.provider.ts
@@ -2,10 +2,16 @@ import Configuration from "./configuration";
 
 function configurationProvider(): Configuration {
   return {
+    endpoint: process.env.AWS_S3_ENDPOINT,
     apiVersion: process.env.AWS_S3_API_VERSION,
     bucketName: process.env.AWS_S3_BUCKET_NAME ?? "test/assets",
     region: process.env.AWS_S3_REGION,
-    useMock: Boolean(process.env.AWS_S3_USE_MOCK ?? true),
+    s3ForcePathStyle: process.env.AWS_S3_FORCE_PATH_STYLE
+      ? process.env.AWS_S3_FORCE_PATH_STYLE === "true"
+      : undefined,
+    useMock: process.env.AWS_S3_USE_MOCK
+      ? process.env.AWS_S3_USE_MOCK === "true"
+      : true,
   };
 }
 export default configurationProvider;
