6f8950f3f302a6bd0cbe25c6eae923e935ec1a37
Ara Park <kdPark0723@gmail.com>
2020-12-03 19:39:29 +0900

    feat: fix lint (#37)

--- packages/s3-media/src/service/service-dependency.initializer.ts
+++ packages/s3-media/src/service/service-dependency.initializer.ts
@@ -20,7 +20,6 @@ class ServiceDependencyInitializer implements Initializer {
     autoInjected(Exchanger)
   );
 
-
   private readonly fileInfoRepositoryProvider = inContainerScope(
     autoInjected(FileInfoRepository)
   );
